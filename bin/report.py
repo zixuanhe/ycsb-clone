08f7b70e132d84389d2827e3da486383b282cfc2
Denis Nelubin <dnelubin@thumbtack.net>
2012-10-25 12:32:06 +0700

    Finished script to parse reports

--- bin/report.py
+++ bin/report.py
@@ -4,36 +4,51 @@
 
 import sys,re
 
-pattern = re.compile(r'\[([^,]*)\],\s*([^\(,\d]{3,})[^,]*,\s*([0-9\.]+)')
+pattern = re.compile(r'\[([^,]*)\],\s*(\d*[^\(,\d=]{3,})[^,]*,\s*([0-9\.]+)')
 
 results = {}
+out = []
 
 for line in sys.stdin:
     match = pattern.match(line)
     if match != None:
 	#print '%s\t%s\t%s' % (match.group(1), match.group(2), match.group(3))
-	results[match.group(2)] = match.group(3)
+	key = match.group(2)
+	while (results.has_key(key)):
+		key += '_'
+	results[key] = match.group(3)
 
-print results
+#print results
 
 def toMillis(value):
 	return str(float(value)/1000.0)
+	
+def appendString(key):
+	if results.has_key(key):
+		out.append(results[key])
+	else:
+		out.append('')
+		
+def appendMicros(key):
+	if results.has_key(key):
+		out.append(toMillis(results[key]))
+	else:
+		out.append('')
 
-out = []
-out.append(results['RunTime'])
-out.append(results['Throughput'])
+appendString('RunTime')
+appendString('Throughput')
 out.append('')
-out.append(results['Operations'])
-out.append(toMillis(results['AverageLatency']))
-out.append(toMillis(results['MinLatency']))
-out.append(toMillis(results['MaxLatency']))
-if results.has_key('95thPercentileLatency'):
-	out.append(toMillis(results['95thPercentileLatency']))
-else:
-	out.append('')
-if results.has_key('99thPercentileLatency'):
-	out.append(toMillis(results['99thPercentileLatency']))
-else:
-	out.append('')	
+appendString('Operations')
+appendMicros('AverageLatency')
+appendMicros('MinLatency')
+appendMicros('MaxLatency')
+appendString('95thPercentileLatency')
+appendString('99thPercentileLatency')
+appendString('Operations_')
+appendMicros('AverageLatency_')
+appendMicros('MinLatency_')
+appendMicros('MaxLatency_')
+appendString('95thPercentileLatency_')
+appendString('99thPercentileLatency_')
 
 print '\t'.join(out)
