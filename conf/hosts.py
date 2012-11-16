31e3c972b0612edeae78af6524662c76efe5f49b
Denis Nelubin <dnelubin@thumbtack.net>
2012-11-16 13:07:20 +0700

    Added R5 host.

--- conf/hosts.py
+++ conf/hosts.py
@@ -8,7 +8,7 @@ env.show = ['debug']
 
 env.roledefs = {
     'client': ['c1.citrusleaf.local', 'c2.citrusleaf.local', 'c3.citrusleaf.local', 'c4.citrusleaf.local'],
-    'server': ['r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r4.citrusleaf.local'],
+    'server': ['r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r5.citrusleaf.local'],
 }
 
 timezone = pytz.timezone('US/Pacific')
