00a1cd419e61a389f03b7fd5b8d8f71e95d498ac
Nick Linker <nlinker@thumbtack.net>
2012-12-03 19:09:37 +0700

    Refs #20495 implemented preemtive approach

--- fabfile/series.py
+++ fabfile/series.py
@@ -1,45 +1,48 @@
 #!/usr/bin/python
+from datetime import timedelta
 import re
 from fabric import tasks
-from time import sleep
-from fabric.context_managers import hide, settings, cd
+from fabric.context_managers import cd
 from fabric.network import disconnect_all
 from fabric.operations import run, put
 from conf.hosts import env
+from conf import workloads
 from fabfile.helpers import get_db, get_workload, _at, base_time, almost_nothing, get_outfilename
 
-env.user = 'vagrant'
-env.password = 'vagrant'
-#servers = env.roledefs['server']
+#env.user = 'vagrant'
+#env.password = 'vagrant'
+#timezone = pytz.timezone('CET')
 #clients = ['192.168.0.11', '192.168.0.12', '192.168.0.13', '192.168.0.14']
-clients = ['192.168.8.108', '192.168.9.213', '192.168.8.41', '192.168.8.118']
-# lock file name, it shows that the test is still running
-lock = 'run_workload.lock'
-# benchmark file name, it loads the CPU to consume time
-benchmark = 'n-body.py'
+#clients = ['192.168.8.108', '192.168.9.213', '192.168.8.41', '192.168.8.118']
+clients = env.roledefs['client']
+# benchmark file name, it bothers the CPU and consumes time and energy
+benchmark = 'execute.sh'
 
-def prepare_ycsbruncmd(database, workload, the_time, param):
-#    cmd = 'touch %s && chmod 444 %s' % (lock, lock)
-#    cmd += workloads.root + '/bin/ycsb run %s -s' % database['command']
-#    for file in workload['propertyfiles']:
-#        cmd += ' -P %s' % file
-#    for (key, value) in database['properties'].items():
-#        cmd += ' -p %s=%s' % (key, value)
-#    for (key, value) in workloads.data.items():
-#        if key == 'operationcount':
-#            cmd += ' -p %s=%s' % (key, int(value) / totalclients)
-#        else:
-#            cmd += ' -p %s=%s' % (key, value)
-#    if target is not None:
-#        cmd += ' -target %s' % str(target)
-    cmd = 'touch %s && chmod 444 %s' % (lock, lock)
-    cmd += ' && ./%s %s' % (benchmark, param)
-    outfile = get_outfilename(database['name'], workload['name'], 'out', the_time, param)
-    errfile = get_outfilename(database['name'], workload['name'], 'err', the_time, param)
+def prepare_ycsbruncmd(hosts, database, workload, the_time, target):
+    # /opt/ycsb/bin/ycsb run couchbase ... -target 25000
+    # and we assign
+    # $1 -> run couchbase -s -P /opt/ycsb/workloads/workloada -p couchbase.user= -p couchbase.bucket=test -p couchbase.opTimeout=60000 -p couchbase.checkOperationStatus=true -p couchbase.password= -p couchbase.hosts=e1.citrusleaf.local,e2.citrusleaf.local,e3.citrusleaf.local,e4.citrusleaf.local -p fieldnameprefix=f -p recordcount=50000000 -p fieldcount=10 -p retrydelay=1 -p threadcount=32 -p readretrycount=1000 -p fieldlength=10 -p exportmeasurementsinterval=30000 -p workload=com.yahoo.ycsb.workloads.CoreWorkload -p updateretrycount=1000 -p insertretrycount=1000000 -p warmupexecutiontime=60000 -p operationcount=2500000
+    # $2 -> -target 25000
+    # $2 could be empty
+    par = '' # /opt/ycsb/bin/ycsb is hardcoded in the benchmark file
+    par += ' run %s -s' % database['command']
+    for file in workload['propertyfiles']:
+        par += ' -P %s' % file
+    for (key, value) in database['properties'].items():
+        par += ' -p %s=%s' % (key, value)
+    for (key, value) in workloads.data.items():
+        if key == 'operationcount':
+            par += ' -p %s=%s' % (key, int(value) / len(hosts))
+        else:
+            par += ' -p %s=%s' % (key, value)
+    if target is not None:
+        par += ' -target %s' % str(target)
+    # parameters are constructed
+    outfile = get_outfilename(database['name'], workload['name'], 'out', the_time, target)
+    errfile = get_outfilename(database['name'], workload['name'], 'err', the_time, target)
+    cmd = './%s %s' % (benchmark, par)
     cmd += ' > %s/%s' % (database['home'], outfile)
     cmd += ' 2> %s/%s' % (database['home'], errfile)
-    # remove lock in any case
-    cmd += ' ; rm -f %s' % lock
     return cmd
 
 def initialize(hosts, db):
@@ -49,80 +52,73 @@ def initialize(hosts, db):
     database = get_db(db)
     def inner_initialize():
         with cd(database['home']):
-            # if the file do exist, then the host is NOT ready => False
-            # returns a map from hosts to True or False
-            run('rm -rf ./*')
+            # run('rm -rf ./*')
+            # clear all the tasks that submitted so far
+            tasks = run('atq').split('\r\n')
+            tid = []
+            for task in tasks:
+                m = re.search('^(\d+)\t', task)
+                if m:
+                    tid.append(m.group(1))
+            run('atrm %s' % ' '.join(tid))
             put(benchmark, benchmark, mode=0744)
+            put('nbody.py', 'nbody.py')
+            # put('nbody.java', 'nbody.java')
+            # run('javac nbody.java')
             print 'host %s initialized ' % env.host
     with almost_nothing():
         tasks.execute(inner_initialize, hosts=hosts)
 
-def poll_hosts(hosts, db):
+def submit_workload(hosts, db, workload, the_time, target = None):
     """
-    Returns a map from host to pairs (no_file, no_jobs), each pair
-    is the pair of booleans means that is there the lock file and
-    are there any jobs scheduled.
-    """
-    database = get_db(db)
-    rs = dict()
-    def inner_poll_hosts():
-        with cd(database['home']):
-            # if the file do exist, then the host is NOT ready => False
-            no_file = bool(run('[ -f %s ]' % lock).return_code)
-            # if are there jobs scheduled, then the host is NOT ready
-            pj = re.compile('\d+\s+.*')
-            no_jobs = not pj.match(run('atq'))
-            rs[env.host] = (no_file, no_jobs)
-    with almost_nothing():
-        tasks.execute(inner_poll_hosts, hosts=hosts)
-    return rs
-
-def infinite_wait(hosts, db, delay = 10):
-    """
-    Waits until all the servers become ready, delay in seconds
-    """
-    while True:
-        rs = poll_hosts(hosts, db)
-        locked    = [it[0] for it in rs.items() if it[1][0] == False]
-        scheduled = [it[0] for it in rs.items() if it[1][1] == False]
-        if not locked and not scheduled:
-            break
-        print "Awaiting: locked %s; sheduled %s" % (locked, scheduled)
-        sleep(delay)
-
-def run_workload(hosts, db, workload, target = None):
-    """
-    Starts running of the workload.
+    Schedules the workload.
     Note: we cannot use ycsb.workload, because it is decorated
     """
     database = get_db(db)
     load = get_workload(workload)
-    the_time = base_time()
-    def inner_run_workload():
+    def inner_submit_workload():
         with cd(database['home']):
             param = int(target) / len(hosts) if target is not None else None
             # command = prepare_ycsbruncmd(database, load, the_time, param)
-            command = _at(prepare_ycsbruncmd(database, load, the_time, param), the_time)
+            command = _at(prepare_ycsbruncmd(hosts, database, load, the_time, param), the_time)
             run(command)
+
     with almost_nothing():
-        tasks.execute(inner_run_workload, hosts=hosts)
-    return the_time
+        tasks.execute(inner_submit_workload, hosts=hosts)
+
+def delay(t):
+    """ Returns estimated delay (run time) for the test with parameter t.
+    In seconds """
+    # get operation count
+    opc = workloads.data['operationcount']
+    t = opc if t is None else t
+    d = int((opc / t) * 1.1)
+    return timedelta(seconds = d)
 
 def run_test_series(db, workload, seq):
     """ This script takes a sequence of threshold values and executes tests """
     initialize(clients, db)
+    the_time = base_time()
     for t in seq:
-        t = None if t == 0 else t
-        # wait until all the servers be ready
-        infinite_wait(clients, db)
-        # fab ycsb_run:db=aerospike,workload=A,target=200000
-        the_time = run_workload(clients, db, workload, t)
-        print "submit tests with threshold = %s on %s" % (t, the_time)
-        # end of all
+        t = t if t > 0 else None
+        # submit the task
+        submit_workload(clients, db, workload, the_time, t)
+        print "submitted on %s with threshold = %s" % (the_time, t)
+        # the_time += timedelta(seconds = 10)
+        the_time += delay(t)
+        the_time = base_time(the_time) # round the time up
+    # end of all
     disconnect_all()
 
 if __name__ == "__main__":
+    # hardcoded
     db = 'couchbase'
     workload = 'A'
-    seq = map(lambda t: t * 1000, [1, 2, 4, 6, 8, 10, 15, 20, 25, 50, 75, 100, 125, 150, 175, 200, 0])
+    # from command line
+    # db = sys.argv[1]
+    # workload = sys.argv[2]
+
+    # 0 means infinity
+#    seq = map(lambda t: t * 1000, [1, 2, 4, 6, 8, 10, 15, 20, 25, 50, 75, 100, 0])
+    seq = map(lambda t: t * 1000, [100, 200, 0])
     run_test_series(db, workload, seq)
