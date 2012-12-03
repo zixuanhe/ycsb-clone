c7035545d5a92bebd44f70ab31ce86d309a56182
Denis Nelubin <dnelubin@thumbtack.net>
2012-12-03 20:57:04 +0700

    Added ability to define properties for workloads.

--- conf/workloads.py
+++ conf/workloads.py
@@ -1,6 +1,6 @@
 root = '/opt/ycsb'
 
-data = {
+data = {    #global YSCB properties
     #'recordcount': 500000000,  #SSD
     'recordcount': 50000000,    #RAM
     'fieldcount': 10,
@@ -10,7 +10,7 @@ data = {
     'threadcount': 32,
     'workload': 'com.yahoo.ycsb.workloads.CoreWorkload',
     'exportmeasurementsinterval': 30000,
-    'warmupexecutiontime': 60000,
+    #'warmupexecutiontime': 60000,
     'insertretrycount': 1000000,
     'readretrycount': 1000,
     'updateretrycount': 1000,
@@ -21,11 +21,19 @@ data = {
 
 workloads = {
     'A': {
-        'name': 'workloada',
-        'propertyfiles': [ root + '/workloads/workloada' ],
+        'name': 'workloada',    #name of the workload to be part of the log files
+        'propertyfiles': [ root + '/workloads/workloada' ], #workload properties files
     },
     'B': {
         'name': 'workloadb',
         'propertyfiles': [ root + '/workloads/workloadb' ],
-    }
+    },
+    'C': {
+        'name': 'workloadc',
+        'propertyfiles': [ root + '/workloads/workloadc' ],
+        'properties': {     #additional workload properties, overrides the global ones
+            'operationcount': 10000000,
+            'maxexecutiontime': 60000,
+        },
+    },
 }
