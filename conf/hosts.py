9f1575f0aa431f29735b9e58525077d8dde69a11
Denis Nelubin <dnelubin@gmail.com>
2013-02-13 14:24:24 +0700

    Changed core Client exceptions logging to avoid too large logs.

--- conf/hosts.py
+++ conf/hosts.py
@@ -19,8 +19,8 @@ env.roledefs = {
     'server': ['e1.citrusleaf.local', 'e2.citrusleaf.local', 'e3.citrusleaf.local', 'e4.citrusleaf.local'],
 
     #list of all available client hosts
-    #'all_client': ['c1.citrusleaf.local', 'c2.citrusleaf.local', 'c3.citrusleaf.local', 'c4.citrusleaf.local', 'c5.citrusleaf.local', 'c6.citrusleaf.local', 'r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r5.citrusleaf.local'],
-    'all_client': ['c1.citrusleaf.local', 'c2.citrusleaf.local', 'c3.citrusleaf.local', 'c6.citrusleaf.local', 'r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r5.citrusleaf.local'],
+    'all_client': ['c1.citrusleaf.local', 'c2.citrusleaf.local', 'c3.citrusleaf.local', 'c4.citrusleaf.local', 'c5.citrusleaf.local', 'c6.citrusleaf.local', 'r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r5.citrusleaf.local'],
+    #'all_client': ['c1.citrusleaf.local', 'c2.citrusleaf.local', 'c3.citrusleaf.local', 'c6.citrusleaf.local', 'r1.citrusleaf.local', 'r2.citrusleaf.local', 'r3.citrusleaf.local', 'r5.citrusleaf.local'],
 }
 
 #hosts timezone (required to correctly schedule ycsb tasks)
