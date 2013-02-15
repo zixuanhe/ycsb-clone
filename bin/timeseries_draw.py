26c6d91fd2a13c0199e0d8987cf29d2677056690
Nick Linker <nlinker@thumbtack.net>
2013-02-15 10:33:29 +0700

    Refs #22389 1st version of scripts to draw latency/throughput

--- bin/timeseries_draw.py
+++ bin/timeseries_draw.py
@@ -1,29 +1,75 @@
 #!/usr/bin/python
+import csv
 
 import sys
-import pkg_resources
 
-pkg_resources.require("matplotlib==1.3.x")
+# import pkg_resources
+# pkg_resources.require("matplotlib==1.3.x")
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-TEST_TIME = 40 * 60 * 1000 #40 Mins in ms
+# TEST_TIME = 40 * 60 * 1000 #40 Mins in ms
+from numpy import arange
 
 
-def f(x_range, dict_to_draw):
-    for p in x_range:
-        yield dict_to_draw.get(p, 0) 
-    
+def f(x, dict_to_draw):
+    dict_to_draw.get(x, 0)
+    # for p in x_range:
+    #     yield dict_to_draw.get(p, 0)
+
+
+def load_series(fin):
+    # all series is sorted, there is no need to use dictionaries
+    draw_rlat = [[],[]] # x and y for read latency
+    draw_ulat = [[],[]] # x and y for update latency
+    draw_thr  = [[],[]] # x and y for throughput
+    # the block number, 0 - read, 1 - update, 2 - throughput
+    # see /timeseries_merge.py:70
+    block = 0
+    reader = csv.reader(fin, dialect='excel-tab')
+    for items in reader:
+        if len(items) < 2:
+            block += 1
+        else:
+            if block == 0:
+                draw_rlat[0].append(int(items[0]))
+                draw_rlat[1].append(float(items[1]))
+            elif block == 1:
+                draw_ulat[0].append(int(items[0]))
+                draw_ulat[1].append(float(items[1]))
+            else:
+                draw_thr[0].append(int(items[0]))
+                draw_thr[1].append(float(items[1]))
+    # dead birds falling from the sky...
+    # maybe use dict?
+    return (draw_rlat, draw_ulat, draw_thr)
 
 if __name__ == "__main__":
-    to_draw = {}
-    for line in sys.stdin.readlines():
-        items = line.split('\t')
-        to_draw[int(items[0])] = float(items[1])
-        
-    x = xrange(0, TEST_TIME, 100)
-    g = f(x, to_draw)
+    if len(sys.argv) > 1:
+        # filename passed
+        with open(sys.argv[1]) as fin:
+            (draw_rlat, draw_ulat, draw_thr) = load_series(fin)
+    else:
+        # from stdin
+        (draw_rlat, draw_ulat, draw_thr) = load_series(sys.stdin)
 
-    plt.plot(x, [y for y in g], 'r')
-    plt.savefig('out.svg')#, transparent=True, bbox_inches='tight', pad_inches=0)
-    
+    min_x = min(min(draw_rlat[0]), min(draw_ulat[0]), min(draw_thr[0]))
+    max_x = max(max(draw_rlat[0]), max(draw_ulat[0]), max(draw_thr[0]))
+
+    plt.subplot(211)
+    plt.grid(True)
+    plt.plot(draw_rlat[0], draw_rlat[1], 'b', draw_ulat[0], draw_ulat[1], 'g')
+    plt.xlim([min_x, max_x])
+    plt.ylim([0, 10])
+    plt.xlabel('Execution time (ms)')
+    plt.ylabel('Latency (ms^(-1))')
+
+    plt.subplot(212)
+    plt.plot(draw_thr[0], draw_thr[1], 'r')
+    plt.xlim([min_x, max_x])
+    plt.xlabel('Execution time (ms)')
+    plt.ylabel('Throughput (messages/sec)')
+
+    # plt.show()
+    plt.savefig("series.png")
--- bin/timeseries_merge.py
+++ bin/timeseries_merge.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import os
 import re
 
-from collections  import OrderedDict
+import sys
 
 def avg(seq):
     return sum(seq) / float(len(seq))
@@ -14,22 +15,21 @@ def scale1k(x) : return x / 1000.0
 
 def merge():
     """grab all *.out, extract statistics from there and merge into TSV file """
-    throughput = OrderedDict()
+    throughput = {}
     update_latency = {}
     read_latency = {}
     items = filter(lambda x: str(x).endswith('.out'), os.listdir('.'))
     # gather stats from all files=items
     for item in items:
-        with open(item) as file:
-            for line in file:
+        with open(item) as f:
+            for line in f:
                 if line.startswith('[UPDATE]') or line.startswith('[READ]'):
                     items = line.split(',')
                     if len(items) == 4:
-                        (op, timestamp, lat, thr) = items 
+                        (op, timestamp, lat, thr) = items
                         timestamp = int(timestamp)
                         #if timestamp == 0:
                         #   print items
-
                         lat = float(lat) / 1000.0
                         thr = thr.strip().split(' ', 1)[0]
                         try:
@@ -37,16 +37,52 @@ def merge():
                         except ValueError:
                             #For "[UPDATE], 1575400, 16432.262857142858, 5250.0Reconnecting to the DB..." line
                             thr = float(re.search('\d+\.\d+', thr).group(0))
-                        thr_stats = throughput.get(timestamp)
-                            
-                        #TODO latency stats
-                        if not thr_stats:
-                            thr_stats = 0
+                        thr_stats = throughput.get(timestamp, 0)
                         thr_stats += thr
                         throughput[timestamp] = thr_stats
+                        # default latensy 0 will not work properly
+                        # hence this statement is totally wrong:
+                        if (op == '[READ]'):
+                            stats1 = read_latency.get(timestamp, [])
+                            stats1.append(lat)
+                            read_latency[timestamp] = stats1
+                        elif (op == '[UPDATE]'):
+                            stats2 = update_latency.get(timestamp, [])
+                            stats2.append(lat)
+                            update_latency[timestamp] = stats2
     #for (timestamp, thr) in OrderedDict(sorted(throughput.items(), key=lambda t: t[0])).items():
-    for (timestamp, thr) in throughput.items():             
-        print(tab_str((timestamp, thr)))
+    if len(sys.argv) > 1:
+        # filename passed, to filename
+        with open(sys.argv[1], 'w') as f:
+            flush_series(f, read_latency, update_latency, throughput)
+    else:
+        # to stdout
+        flush_series(sys.stdout, read_latency, update_latency, throughput)
+
+def avg_convert(the_dict):
+    # post-processing for latencies goes here
+    list1 = the_dict.items()
+    list2 = [(x, avg(y)) for (x, y) in list1 if len(y) > 0]
+    # remove empty elements
+    list2.sort(key=lambda (x, y): x)
+    # convert each (sub)list to average
+    return list2
+
+
+def flush_series(f, read_latency, update_latency, throughput):
+    for t in avg_convert(read_latency):
+        print(tab_str(t), file=f)
+        # skip line to mark the end of the block
+    print('', file=f)
+    for t in avg_convert(update_latency):
+        print(tab_str(t), file=f)
+        # skip line to mark the end of the block
+    print('', file=f)
+    # sort for timeseries.draw
+    thr_list = throughput.items()
+    thr_list.sort(key=lambda (x, y): x)
+    for t in thr_list:
+        print(tab_str(t), file=f)
 
 def tab_str(seq):
     return '\t'.join(map(str, seq))
