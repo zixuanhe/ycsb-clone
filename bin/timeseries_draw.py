0af816d88664cd90b63d985b866f85761d20f858
Nick Linker <nlinker@thumbtack.net>
2013-02-15 16:13:17 +0700

    Refs #22389 + legend

--- bin/timeseries_draw.py
+++ bin/timeseries_draw.py
@@ -7,10 +7,7 @@ import sys
 # pkg_resources.require("matplotlib==1.3.x")
 
 import matplotlib.pyplot as plt
-import numpy as np
-
-# TEST_TIME = 40 * 60 * 1000 #40 Mins in ms
-from numpy import arange
+from matplotlib.font_manager import FontProperties
 
 
 def f(x, dict_to_draw):
@@ -21,9 +18,9 @@ def f(x, dict_to_draw):
 
 def load_series(fin):
     # all series is sorted, there is no need to use dictionaries
-    draw_rlat = [[],[]] # x and y for read latency
-    draw_ulat = [[],[]] # x and y for update latency
-    draw_thr  = [[],[]] # x and y for throughput
+    drlt = [[],[]] # x and y for read latency
+    dult = [[],[]] # x and y for update latency
+    dthr = [[],[]] # x and y for throughput
     # the block number, 0 - read, 1 - update, 2 - throughput
     # see /timeseries_merge.py:70
     block = 0
@@ -33,44 +30,48 @@ def load_series(fin):
             block += 1
         else:
             if block == 0:
-                draw_rlat[0].append(int(items[0]))
-                draw_rlat[1].append(float(items[1]))
+                drlt[0].append(int(items[0]))
+                drlt[1].append(float(items[1]))
             elif block == 1:
-                draw_ulat[0].append(int(items[0]))
-                draw_ulat[1].append(float(items[1]))
+                dult[0].append(int(items[0]))
+                dult[1].append(float(items[1]))
             else:
-                draw_thr[0].append(int(items[0]))
-                draw_thr[1].append(float(items[1]))
+                dthr[0].append(int(items[0]))
+                dthr[1].append(float(items[1]))
     # dead birds falling from the sky...
     # maybe use dict?
-    return (draw_rlat, draw_ulat, draw_thr)
+    return (drlt, dult, dthr)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         # filename passed
         with open(sys.argv[1]) as fin:
-            (draw_rlat, draw_ulat, draw_thr) = load_series(fin)
+            (drlt, dult, dthr) = load_series(fin)
     else:
         # from stdin
-        (draw_rlat, draw_ulat, draw_thr) = load_series(sys.stdin)
+        (drlt, dult, dthr) = load_series(sys.stdin)
 
-    min_x = min(min(draw_rlat[0]), min(draw_ulat[0]), min(draw_thr[0]))
-    max_x = max(max(draw_rlat[0]), max(draw_ulat[0]), max(draw_thr[0]))
+    min_x = min(min(drlt[0]), min(dult[0]), min(dthr[0]))
+    max_x = max(max(drlt[0]), max(dult[0]), max(dthr[0]))
 
     plt.subplot(211)
     plt.grid(True)
-    plt.plot(draw_rlat[0], draw_rlat[1], 'b', draw_ulat[0], draw_ulat[1], 'g')
+    plt.plot(dthr[0], dthr[1], 'r')
     plt.xlim([min_x, max_x])
-    plt.ylim([0, 10])
     plt.xlabel('Execution time (ms)')
-    plt.ylabel('Latency (ms^(-1))')
+    plt.ylabel('Throughput (msgs/sec)')
 
-    plt.subplot(212)
+    ax = plt.subplot(212)
     plt.grid(True)
-    plt.plot(draw_thr[0], draw_thr[1], 'r')
+    p1, = plt.plot(drlt[0], drlt[1], 'b', label = 'Read latency')
+    p2, = plt.plot(dult[0], dult[1], 'g', label = 'Update latency')
     plt.xlim([min_x, max_x])
+    plt.ylim([0, 5])
     plt.xlabel('Execution time (ms)')
-    plt.ylabel('Throughput (messages/sec)')
+    plt.ylabel('Latency (ms)')
+    fontP = FontProperties()
+    fontP.set_size('small')
+    ax.legend(prop = fontP)
 
     # plt.show()
-    plt.savefig("series.png")
+    plt.savefig("series.svg")
